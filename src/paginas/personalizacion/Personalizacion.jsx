d20b4b7a07103fae4294d292c75b6b51a8edc0d8
Guillermo Enguita Lahoz <90970380+Guilleuz@users.noreply.github.com>
2022-05-12 15:38:00 +0200

    Modificada pantalla de personalización, para evitar llamada a la API al actualizar los objetos equipados.

--- src/paginas/personalizacion/Personalizacion.jsx
+++ src/paginas/personalizacion/Personalizacion.jsx
@@ -80,23 +80,17 @@ export default class Personalizacion extends React.Component {
             let avataresArr = [];
             for (var i=0; i < Object.keys(response).length; i++) {
                 if (response[i]["Tipo"] === "dado") {
-                    dadosArr.push(<div className="dado" key={response[i]["Id"]}>
-                        {response[i]["Nombre"]}, {response[i]["Descripcion"]}
-                        {this.state.dadoEquipado == response[i]["Id"] && "Objeto equipado"}
-                        {this.state.dadoEquipado != response[i]["Id"] && 
-                            <button id={response[i]["Id"]} onClick={(e) => this.equiparCosmetico(e)}>Equipar dado</button>}
-                    </div>)
+                    let dado = {id: response[i]["Id"], nombre: response[i]["Nombre"], descripcion: response[i]["Descripcion"], img: response[i]["Imagen"]}
+                    dadosArr.push(dado);
                 }
                 else {
-                    avataresArr.push(<div className="avatar" key={response[i]["Id"]}>
-                        {response[i]["Nombre"]}, {response[i]["Descripcion"]}
-                        {this.state.avatarEquipado == response[i]["Id"] && "Objeto equipado"}
-                        {this.state.avatarEquipado != response[i]["Id"] && 
-                            <button id={response[i]["Id"]} onClick={(e) => this.equiparCosmetico(e)}>Equipar avatar</button>}
-                    </div>)
+                    let avatar = {id: response[i]["Id"], nombre: response[i]["Nombre"], descripcion: response[i]["Descripcion"],img: response[i]["Imagen"]};
+                    avataresArr.push(avatar);
                 }
             }
 
+            dadosArr.forEach((x,i) => console.log(x));
+            avataresArr.forEach((x,i) => console.log(x));
             this.setState({dados: dadosArr, avatares:avataresArr});
         })
         .catch((e) => {
@@ -123,7 +117,6 @@ export default class Personalizacion extends React.Component {
             // Recargamos los cosméticos del jugador
             let usuario = this.getNombreUsuario(document.cookie);
             this.consultarEquipacion(usuario);
-            this.consultarColeccion(usuario);
         })
         .catch((e) => {
             swal.fire({
@@ -135,6 +128,28 @@ export default class Personalizacion extends React.Component {
     }
 
     render() {
+        let dadosArr = [];
+        let avataresArr = [];
+        
+        // Creamos la lista de dados a mostrar
+        for (var i=0; i < this.state.dados.length; i++) {
+            dadosArr.push(<div className="dado" key={this.state.dados[i].id}>
+                {this.state.dados[i].nombre}, {this.state.dados[i].descripcion}
+                {this.state.dadoEquipado == this.state.dados[i].id && "Objeto equipado"}
+                {this.state.dadoEquipado != this.state.dados[i].id && 
+                    <button id={this.state.dados[i].id } onClick={(e) => this.equiparCosmetico(e)}>Equipar dado</button>}
+            </div>)
+        }
+
+        // Creamos la lista de avatares a mostrar
+        for (var i=0; i < this.state.avatares.length; i++) {
+            avataresArr.push(<div className="avatar" key={this.state.avatares[i].id}>
+                {this.state.avatares[i].nombre}, {this.state.avatares[i].descripcion}
+                {this.state.avatarEquipado == this.state.avatares[i].id && "Objeto equipado"}
+                {this.state.avatarEquipado != this.state.avatares[i].id && 
+                    <button id={this.state.avatares[i].id} onClick={(e) => this.equiparCosmetico(e)}>Equipar avatar</button>}
+            </div>)
+        }
         
 
         return (
@@ -145,8 +160,8 @@ export default class Personalizacion extends React.Component {
             <button onClick={() => {this.setState({mostrarAvatares: false, mostrarDados: true})}}>Dados</button>
 
             <div className="equipacionActual">Avatar equipado {this.state.avatarEquipado}, dado {this.state.dadoEquipado}</div>
-            {this.state.mostrarAvatares && <div className="avatares">Avatares {this.state.avatares}</div>}
-            {this.state.mostrarDados && <div className="dados">Dados {this.state.dados}</div>}
+            {this.state.mostrarAvatares && <div className="avatares">Avatares {avataresArr}</div>}
+            {this.state.mostrarDados && <div className="dados">Dados {dadosArr}</div>}
             <BarraInferior></BarraInferior>
         </div>
         );  
